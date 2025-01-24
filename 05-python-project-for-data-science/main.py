acf570704be5e968e87761979e279223d5841242
ibrahim-kocyigit <ibrahimkocyigit@gmail.com>
2025-01-24 11:11:30 +0300

    finishes assignment for course 05

--- 05-python-project-for-data-science/main.py
+++ 05-python-project-for-data-science/main.py
@@ -1,7 +1,25 @@
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
-url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/netflix_data_webpage.html"
+URL = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/revenue.htm"
 
-data = pd.read_html(url)
+html_data = requests.get(URL).text
 
-type(data[0])
+soup = BeautifulSoup(html_data, "html.parser")
+
+tesla_revenue = pd.read_html(str(soup))[0]
+tesla_revenue.rename(
+    columns={
+        "Tesla Annual Revenue (Millions of US $)": "Date",
+        "Tesla Annual Revenue (Millions of US $).1": "Revenue",
+    },
+    inplace=True,
+)
+
+tesla_revenue["Revenue"] = (
+    tesla_revenue["Revenue"]
+    .str.replace(",", "")
+    .str.replace("$", "")
+    .apply(pd.to_numeric)
+)
