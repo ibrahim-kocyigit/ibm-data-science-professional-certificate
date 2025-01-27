518bdbdc7fc896a87f545b83e869001cac1941b7
ibrahim-kocyigit <ibrahimkocyigit@gmail.com>
2025-01-27 10:57:35 +0300

    finishes deep work session

--- 05-python-project-for-data-science/main.py
+++ 05-python-project-for-data-science/main.py
@@ -8,18 +8,38 @@ html_data = requests.get(URL).text
 
 soup = BeautifulSoup(html_data, "html.parser")
 
-tesla_revenue = pd.read_html(str(soup))[0]
+tesla_revenue = pd.read_html(str(soup))[1]
 tesla_revenue.rename(
     columns={
-        "Tesla Annual Revenue (Millions of US $)": "Date",
-        "Tesla Annual Revenue (Millions of US $).1": "Revenue",
+        "Tesla Quarterly Revenue (Millions of US $)": "Date",
+        "Tesla Quarterly Revenue (Millions of US $).1": "Revenue",
     },
     inplace=True,
 )
 
 tesla_revenue["Revenue"] = (
-    tesla_revenue["Revenue"]
-    .str.replace(",", "")
-    .str.replace("$", "")
-    .apply(pd.to_numeric)
+    tesla_revenue["Revenue"].str.replace(",", "").str.replace("$", "")
 )
+
+tesla_revenue
+
+URL_2 = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/stock.html"
+
+html_data_2 = requests.get(URL_2).text
+
+soup_2 = BeautifulSoup(html_data_2, "html.parser")
+
+gme_revenue = pd.read_html(str(soup_2))[1]
+gme_revenue.rename(
+    columns={
+        "GameStop Quarterly Revenue (Millions of US $)": "Date",
+        "GameStop Quarterly Revenue (Millions of US $).1": "Revenue",
+    },
+    inplace=True,
+)
+
+gme_revenue["Revenue"] = (
+    gme_revenue["Revenue"].str.replace(",", "").str.replace("$", "")
+)
+
+gme_revenue
---
+++ 06-databases-and-sql-for-data-science-with-python/notes.md
@@ -0,0 +1,17 @@
+# DATABASES AND SQL FOR DATA SCIENCE WITH PYTHON
+
+## Table of Contents
+1. [Module 1: ](#module-1)
+2. [Module 2: ](#module-2)
+3. [Module 3: ](#module-3)
+4. [Module 4: ](#module-4)
+5. [Module 5: ](#module-5)
+6. [Module 6: ](#module-6)
+
+# Module 1: Getting Started with SQL <a name="module-1"></a>
+
+## Basic SQL
+
+### Introduction to Databases
+
+
